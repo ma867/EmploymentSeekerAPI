454817b54482fdb341b1a51749c4be883cbfa5fb
ma867 <ma867@njit.edu>
2022-11-09 19:30:01 -0500

    added boiler plate code for express application

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const PORT = process.env.PORT || 8000
+
+//----------CREATE EXPRESS APP----------//
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+
+//----------TEST EXPRESS----------//
+app.get('/', (req,res)=>{
+    res.send("YERRRRR IT'S WORKING")
+})
+
+
+
+
+
+//----------LISTENER----------//
+app.listen(PORT, () => {
+    console.log('yerrrr its running on', PORT)
+})
+
