b7ee406c38adcf93bd8dd4011960432132327d6a
Lovedy Louis-Jean <lovedylouisjean@LovedysacStudio.lan1>
2022-11-15 22:23:58 -0500

    second commit

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -7,7 +7,5 @@ const apiController = {
     }
   }
   
-  // We only need Index and Show because we are currently only ever showing a list of fruits
-  // Or we are showing a single fruit
-  // Additional Routes will be added on Tuesday once we incorporate nuanced functionality
+  
   module.exports = apiController
