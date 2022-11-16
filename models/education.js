36f5ac3fe8f4f5749cc788833301794b729ad81f
Jason Ross <jasonorlandoross@gmail.com>
2022-11-15 20:35:24 -0600

    created education model

---
+++ models/education.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+// Education Schema
+const educationSchema = new mongoose.Schema({
+  schoolName: { type: String, required: true },
+  degree: String,
+  currentStatus: Boolean,
+  startMonth: String,
+  startYear: String
+})
+// Make A Model From The Schema
+
+const Education = mongoose.model('Education', educationSchema)
+
+// Export The Model For Use In The App
+
+module.exports = Education
