42876bfa5cdd1839274a5ff41fa06f7818fec4d1
Lovedy Louis-Jean <lovedylouisjean@LovedysacStudio.lan1>
2022-11-15 21:05:03 -0500

    fisrt commit

---
+++ controllers/apiController.js
@@ -0,0 +1,13 @@
+const apiController = {
+    index (req, res, next) {
+      res.json(res.locals.data.employmentSeekerApi)
+    },
+    show (req, res, next) {
+      res.json(res.locals.data.employmentSeekerApi)
+    }
+  }
+  
+  // We only need Index and Show because we are currently only ever showing a list of fruits
+  // Or we are showing a single fruit
+  // Additional Routes will be added on Tuesday once we incorporate nuanced functionality
+  module.exports = apiController
