9af6a56f4dd25c02caf6c97cf28ede4b59af9a0a
Seezee23 <seezee146@yahoo.com>
2022-11-16 19:50:31 -0500

    second commit

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -4,38 +4,41 @@ const dataController = require('./dataController.js')
 const apiController = require('./apiController.js')
 
 // add routes for user model
+// user routes
 // Index
-router.get('/api/user', dataController.userIndex, apiController.index)
+router.get('/api/user', dataController.userIndex, apiController.userIndex)
 // Delete
-router.delete('/api/user/:id', dataController.userDelete, apiController.show)
+router.delete('/api/user/:id', dataController.userDelete, apiController.userShow)
 // Update
-router.put('/api/user/:id', dataController.userUpdate, apiController.show)
+router.put('/api/user/:id', dataController.userUpdate, apiController.userShow)
 // Create
-router.post('/api/user', dataController.userCreate, apiController.show)
+router.post('/api/user', dataController.userCreate, apiController.userShow)
 // Show
-router.get('/api/user/:id', dataController.userShow, apiController.show)
-
-// Index
-router.get('/api/experience', dataController.experienceIndex, apiController.index)
-// Delete
-router.delete('/api/experience/:id', dataController.experienceDelete, apiController.show)
-// Update
-router.put('/api/experience/:id', dataController.experienceUpdate, apiController.show)
-// Create
-router.post('/api/experience', dataController.experienceCreate, apiController.show)
-// Show
-router.get('/api/experience/:id', dataController.experienceShow, apiController.show)
+router.get('/api/user/:id', dataController.userShow, apiController.userShow)
 
 
+// experience routes
 // Index
-router.get('/api/education', dataController.educationIndex, apiController.index)
+router.get('/api/experience', dataController.experienceIndex, apiController.experienceIndex)
 // Delete
-router.delete('/api/education/:id', dataController.educationDelete, apiController.show)
+router.delete('/api/experience/:id', dataController.experienceDelete, apiController.experienceShow)
 // Update
-router.put('/api/education/:id', dataController.educationUpdate, apiController.show)
+router.put('/api/experience/:id', dataController.experienceUpdate, apiController.experienceShow)
 // Create
-router.post('/api/education', dataController.educationCreate, apiController.show)
+router.post('/api/experience', dataController.experienceCreate, apiController.experienceShow)
 // Show
-router.get('/api/education/:id', dataController.educationShow, apiController.show)
+router.get('/api/experience/:id', dataController.experienceShow, apiController.experienceShow)
+
+// education routes
+// Index
+router.get('/api/education', dataController.educationIndex, apiController.educationIndex)
+// Delete
+router.delete('/api/education/:id', dataController.educationDelete, apiController.educationShow)
+// Update
+router.put('/api/education/:id', dataController.educationUpdate, apiController.educationShow)
+// Create
+router.post('/api/education', dataController.educationCreate, apiController.educationShow)
+// Show
+router.get('/api/education/:id', dataController.educationShow, apiController.educationShow)
 
 module.exports = router
