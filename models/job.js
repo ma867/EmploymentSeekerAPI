42b514a6a0f727262eef3b297672cfc30bdf6565
arthurmichaelw <arthurmichaelw@gmail.com>
2022-11-19 12:46:13 -0500

    created the job model

---
+++ models/job.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const jobSchema = new mongoose.Schema({
+    name: { type: String }
+  })
+
+  const Job = mongoose.model('Job', jobSchema)
+
+module.exports = Job
